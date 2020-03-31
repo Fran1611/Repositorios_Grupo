e0344a0dc4afca7840d931989e8ada578a7febbb
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-03-31 19:20:57 -0300

    cambio program

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Repositorios_Grupo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Francisco Ibañez");
         }
     }
 }
