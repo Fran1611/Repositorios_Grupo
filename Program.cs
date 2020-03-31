724af6b6324128dc67b2cc5405bbd120fadeb12f
Fran1611 <francisco.ibanez@correo.ucu.edu.uy>
2020-03-31 19:17:05 -0300

    subo program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Repositorios_Grupo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
